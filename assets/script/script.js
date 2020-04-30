f0dbae0233aa6143aae179bb2f1189e856864a76
Lauren <TechnologicalType@MacBook-Air-2.local>
2020-04-29 20:46:48 -0400

    inital commit

---
+++ assets/script/script.js
@@ -0,0 +1,19 @@
+
+  // Or with jQuery
+
+  $(document).ready(function(){
+    $('.carousel').carousel();
+
+  // $('.carousel.carousel-slider').carousel({
+  //   fullWidth: true
+  // });
+  });
+
+
+
+  // Modals
+
+  $(document).ready(function(){
+    $('.modal').modal();
+  });
+   
